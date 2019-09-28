e738038ab3b10fa078155dc67d54ab534d4a7b3e
Dominic Boisvert <boisvert.dominic@gmail.com>
2019-09-28 14:50:52 -0400

    cleanup

--- rvm2atom.py
+++ rvm2atom.py
@@ -4,19 +4,21 @@
 # Use : python rvm2atom.py argument1 argument2
 # Use : argument1 is the name of the file imported from RVM.
 # Use : argument2 is the name of the output file, must be xml, example : congres.xml
-# Use : add the name of the term in place of AJOUTER LE TERME ICI
+# Use : Add the name of the term in place of AJOUTER LE TERME ICI
 
-import re, sys
+import os, re, sys
 
 rvm_altLabel = []
 linenum = 0
-pattern = re.compile('<skos:altLabel xml:lang="fr">' "[^RVM]" "[^CHS]" "[^RAMEAU]", re.IGNORECASE)
+# Search pattern to find altlabel in RVM XML file. The pattern excludes some altLabels that are institutions.
+pattern = re.compile('<skos:altLabel xml:lang="fr">' "[^AAT]" "[^RVM]" "[^CHS]" "[^MeSH]" "[^RAMEAU]", re.IGNORECASE)
 
 # We create or open atom_file to add some XML stuff at the beginning.
 with open(sys.argv[2], "w+") as atom_file:
     atom_file.write('<?xml version="1.0" encoding="utf-8" ?>' + '\n' + '<rdf:RDF' + '\n' + 'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' + '\n' + 'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' + '\n' + 'xmlns:skos="http://www.w3.org/2004/02/skos/core#"' + '\n' + 'xmlns:dc="http://purl.org/dc/elements/1.1/">' + '\n' + '<skos:prefLabel xml:lang="fr">AJOUTER LE TERME ICI</skos:prefLabel>'  + '\n')
 atom_file.close()
 
+# Open both files to find the altLabels in the input file and copy them in the output file.
 with open(sys.argv[1], 'rt') as rvm_file, \
      open(sys.argv[2], mode='a+') as atom_file:
     for line in rvm_file:
@@ -25,11 +27,11 @@ with open(sys.argv[1], 'rt') as rvm_file, \
             rvm_altLabel.append((linenum, line.rstrip('\n')))
     for altLabel in rvm_altLabel:
         atom_file.write(altLabel[1])
-# We close or files.
+# We close our files.
 atom_file.close()
 rvm_file.close()
 
 # We reopen atom_file to add some XML stuff at the end.
-with open(sys.argv[2], "a") as atom_file2:
-    atom_file2.write('</rdf:RDF>')
-atom_file2.close()
+with open(sys.argv[2], "a") as atom_file:
+    atom_file.write('</rdf:RDF>')
+atom_file.close()
