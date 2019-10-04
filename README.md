aeab5342508baf755ac077d994cafe51b16cbe59
Dominic Boisvert <boisvert.dominic@gmail.com>
2019-10-04 16:17:21 -0400

    Indentation
    Amélioration de la forme du fichier de destination.

--- README.md
+++ README.md
@@ -17,12 +17,14 @@ positional arguments:
 rvm2atom accepte les chemins absolus et relatifs pour la source et la destination.
 
 Example commands:
-rvm2atom.py "/home/bcadmin/Desktop/File from rvm.xml" /home/bcadmin/Desktop/File for atom.xml : le résultat sera un fichier XML presque prêt pour importation dans AtoM.
+rvm2atom.py "/home/bcadmin/Desktop/File from rvm.xml" /home/bcadmin/Desktop/File for atom.xml : le résultat sera un fichier XML presque prêt pour l'importation dans AtoM.
 
 ### Avant l'importation dans AtoM
 
 Le script `rvm2atom.py` va créer une ligne `<skos:prefLabel xml:lang="fr">AJOUTER LE TERME ICI</skos:prefLabel>` dans le fichier de destination.
 
 Vous devrez remplacer le texte `AJOUTER LE TERME ICI` par le terme sujet que vous souhaitez importer dans AtoM.
+
+Vous devez aussi valider les `altLabel` du fichier de destination. Le RVM utilise cette balise pour identifier des sources.
 
 Vous pourrez ensuite importer le fichier XML dans AtoM.
--- rvm2atom.py
+++ rvm2atom.py
@@ -9,7 +9,7 @@ A Python tool to transform RVM XML into something AtoM understands.
 For information on usage and dependencies, see:
 https://github.com/DominicBoisvert/RVM_AtoM
 
-Python 3.4+
+Python 2.7+
 
 The MIT License (MIT)
 Copyright (c) 2019 Dominic Boisvert
@@ -21,11 +21,12 @@ import argparse
 import os
 import re
 import sys
+import textwrap
 
 rvm_altLabel = []
 rvm_prefLabel = []
 
-# Search pattern to find altlabel in RVM XML file. The pattern excludes some altLabels that are unwanted, mainly institutions names. Should be in a config file.
+# Search pattern to find altlabel in RVM XML file. The pattern excludes some altLabels that are unwanted, mainly institutions names it might not catch them all. Should be in a config file.
 pattern = re.compile('<skos:altLabel xml:lang="fr">' "[^AAT]" "[^RVM]" "[^CHS]" "[^MeSH]" "[^RAMEAU]", re.IGNORECASE)
 
 # Search for the last prefLabel in RVM XML file. But doesn't work because the prefLabel we want isn't always the last one.
@@ -43,7 +44,6 @@ def _make_parser(version):
 def main():
     # system info
     rvm2atom_version = 'rvm2atom 0.0.2'
-    linenum = 0
 
     parser = _make_parser(rvm2atom_version)
     args = parser.parse_args()
@@ -53,23 +53,17 @@ def main():
     source = os.path.abspath(args.source)
     destination = os.path.abspath(args.destination)
 
-    # We create or open atom_file to add some XML stuff at the beginning.
+    # We need this for line 67. 
+    linenum = 0
 
+    # We create or open destination file to add some XML stuff at the beginning.
     with open(args.destination, "w+") as destination:
-        destination.write('<?xml version="1.0" encoding="utf-8" ?>' + '\n' + '<rdf:RDF' + '\n' + 'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' + '\n' + 'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' + '\n' + 'xmlns:skos="http://www.w3.org/2004/02/skos/core#"' + '\n' + 'xmlns:dc="http://purl.org/dc/elements/1.1/">' + '\n' '<skos:ConceptS>' + '\n' + '<skos:prefLabel xml:lang="fr">AJOUTER LE TERME ICI</skos:prefLabel>'  + '\n')
+        destination.write('<?xml version="1.0" encoding="utf-8" ?>' + '\n' + '<rdf:RDF' + '\n' + '    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' + '\n' + '    xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' + '\n' + '    xmlns:skos="http://www.w3.org/2004/02/skos/core#"' + '\n' + '    xmlns:dc="http://purl.org/dc/elements/1.1/">' + '\n' + '\n' + '    <skos:ConceptS>' + '\n' + '\n' + '        <skos:prefLabel xml:lang="fr">AJOUTER LE TERME ICI</skos:prefLabel>'  + '\n' + '\n')
     destination.close()
 
-
-    # Open both files to find the altLabels in the input file and copy them in the output file.
+    # Open both files to find the altLabels in the source file and copy them in the destination file. We could have better wrapping.
     with open(args.source, 'rt') as source, \
         open(args.destination, mode='a+') as destination:
-# This code block doesn't work
-#        for line in source:
-#            linenum += 1
-#            if patternPrefLabel.search(line) != None:      # If a match is found
-#                rvm_prefLabel.append((linenum, line.rstrip('\n')))
-#        for prefLabel in rvm_prefLabel:
-#            destination.write(prefLabel[1])
         for line in source:
             linenum += 1
             if pattern.search(line) != None:      # If a match is found
@@ -80,9 +74,9 @@ def main():
     source.close()
     destination.close()
    
-    # We reopen atom_file to add some XML stuff at the end.
+    # We reopen destination file to add some XML stuff at the end.
     with open(args.destination, "a") as destination:
-        destination.write('</skos:Concept>' + '\n' + '</rdf:RDF>')
+        destination.write('    </skos:Concept>' + '\n' + '</rdf:RDF>')
     destination.close()
 
 main()
